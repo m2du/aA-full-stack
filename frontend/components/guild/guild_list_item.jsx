190fb056c2da9c62e861b8a76881264f1ffd8f30
Michael Du <michaeldu.work@gmail.com>
2019-07-08 14:55:38 -0700

    Add server icons

--- frontend/components/guild/guild_list_item.jsx
+++ frontend/components/guild/guild_list_item.jsx
@@ -6,17 +6,20 @@ import DiscordSVG from '../discord_svg';
 class GuildListItem extends React.Component {
   render() {
     const guild = this.props.guild;
-
+    const imageUrl = (guild.imageUrl) ? guild.imageUrl : "";
     return (
       <div className='guild-item'>
         <div className='guild-container'>
-          <Link className='guild-link' to={`/channels/${guild.id}`}>
+          <Link className='guild-link' 
+            style={{backgroundImage: `url(${imageUrl})`}}
+            to={`/channels/${guild.id}`}>
             { 
               (guild.id === 'home') ? 
               <div id='home-svg-wrapper'>
                 <DiscordSVG color='blue' />
               </div> :
-              guild.name
+              (guild.imageUrl) ? null :
+              guild.name.split(' ').map(str => str[0]).join('')
             }
           </Link>
         </div>
